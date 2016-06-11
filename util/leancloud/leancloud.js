639b28ea93d7173774a2fa71d38647145e9a89ae
Mr.Krabs <pangxie.wdy@gmail.com>
2016-06-12 00:49:03 +0800

    feat: 湾区日报的请求, 记录来源ip. （特么家里的天然气罐子泄漏了！卧槽。）

--- util/leancloud/leancloud.js
+++ util/leancloud/leancloud.js
@@ -6,6 +6,7 @@ AV.initialize(leanCloudSecret.appId, leanCloudSecret.appKey);
 
 const Article = AV.Object.extend('Wanqu');
 const Workflows = AV.Object.extend('Workflows');
+const WanquLog = AV.Object.extend('WanquLog');
 
 /* 添加湾区指定某期的数据 */
 module.exports.addArticle = function *(options) {
@@ -42,7 +43,6 @@ module.exports.getSpecArticle = function *(id) {
 
 /* 添加workflow */
 module.exports.addWorkflow = function *(options) {
-
     let WorkflowsObject = new Workflows();
 
     for(let key in options) {
@@ -50,4 +50,11 @@ module.exports.addWorkflow = function *(options) {
     }
 
     WorkflowsObject.save();
+}
+
+/* 添加Wanqu日报搜索内容的日志 */
+module.exports.wanquLog = function *(ip) {
+    let WanquLogObject = new WanquLog();
+    WanquLogObject.set('ip', ip);
+    WanquLogObject.save();
 }
