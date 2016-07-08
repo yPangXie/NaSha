24a03639fbb4784e0c540fb682f871e662d67e1f
Mr.Krabs <pangxie.wdy@gmail.com>
2016-07-08 20:00:52 +0800

    refactor: 所有配置文件移到隐藏的config.js文件中. 并在gitignore文件过滤掉.

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "port": 8000,
-    "routerPrefix": '/ns',
-    "weibo": {
-        "app_key": '',
-        "app_secret": '',
-        "redirect_uri": 'http://bigyoo.me/ns/weibo/authorize'
-    }
-}
