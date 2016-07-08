1edff7b46781cab7f0b2b4758001459978ee6964
Mr.Krabs <pangxie.wdy@gmail.com>
2016-07-08 09:31:03 +0800

    refactor: 移除注释

--- util/leancloud/wanqu.js
+++ util/leancloud/wanqu.js
@@ -3,7 +3,6 @@
 const LeanCloud = require('./initialize');
 
 /* 添加湾区指定某期的数据 */
-// module.exports.addWanqu = function *(options) {
 module.exports.store = function *(options) {
     let wanquObject = new LeanCloud.Wanqu();
     for(let key in options) wanquObject.set(key, options[key]);
@@ -11,7 +10,6 @@ module.exports.store = function *(options) {
 }
 
 /* 获取最新一期的数据 */
-// module.exports.getLatestWanqu = function *() {
 module.exports.getLatest = function *() {
     let wanquQuery = new LeanCloud.AV.Query("Wanqu");
     wanquQuery.descending('createdAt');
@@ -26,7 +24,6 @@ module.exports.getLatest = function *() {
 }
 
 /* 根据期数, 搜索指定的数据 */
-// module.exports.getSpecWanqu = function *(id) {
 module.exports.getSpec = function *(id) {
     let wanquQuery = new LeanCloud.AV.Query("Wanqu");
     wanquQuery.equalTo("season", id);
@@ -50,7 +47,6 @@ module.exports.storeLatestIssueVersion = function *(latestIssue) {
 }
 
 /* 添加Wanqu日报搜索内容的日志 */
-// module.exports.wanquLog = function *(ip) {
 module.exports.log = function *(ip) {
     let WanquLogObject = new LeanCloud.WanquLog();
     WanquLogObject.set('ip', ip);
@@ -58,14 +54,12 @@ module.exports.log = function *(ip) {
 }
 
 /* wanqu日报总数据量 */
-// module.exports.wanquTotal = function *() {
 module.exports.total = function *() {
     let wanquQuery = new LeanCloud.AV.Query('Wanqu');
     return wanquQuery.count();
 }
 
 /* 指定时间点之后的wanqu日志数据总数 */
-// module.exports.wanquLogDaily = function *(date) {
 module.exports.logDailyCount = function *(date) {
     let wanquLogQuery = new LeanCloud.AV.Query('WanquLog');
     wanquLogQuery.greaterThan('createdAt', new Date(date));
@@ -73,7 +67,6 @@ module.exports.logDailyCount = function *(date) {
 }
 
 /* 指定时间点之后的爬取的wanqu日报数据总数 */
-// module.exports.wanquSpiderDaily = function *(date) {
 module.exports.spiderDaily = function *(date) {
     let wanquQuery = new LeanCloud.AV.Query('Wanqu');
     wanquQuery.greaterThan('createdAt', new Date(date));
--- util/leancloud/workflows.js
+++ util/leancloud/workflows.js
@@ -3,7 +3,6 @@
 const LeanCloud = require('./initialize');
 
 /* 添加workflow */
-// module.exports.addWorkflow = function *(options) {
 module.exports.store = function *(options) {
     let WorkflowsObject = new LeanCloud.Workflows();
     for(let key in options) WorkflowsObject.set(key, options[key]);
@@ -11,14 +10,12 @@ module.exports.store = function *(options) {
 }
 
 /* 上传文件 */
-// module.exports.uploadWorkflow = function *(fileName, fileData) {
 module.exports.upload = function *(fileName, fileData) {
     // let file = new LeanCloud.AV.File(fileName, fileData);
     // file.save();
 }
 
 /* 获取当前数据库中, workflow的总数 */
-// module.exports.getCurrentLatestTotalWorkflows = function *() {
 module.exports.getCurrentLatestTotal = function *() {
     let workflowTimingQuery = new LeanCloud.AV.Query("WorkflowTiming");
     workflowTimingQuery.descending('createdAt');
@@ -27,7 +24,6 @@ module.exports.getCurrentLatestTotal = function *() {
 }
 
 /* 存储当前最新的workflow总数 */
-// module.exports.storeLatestTotalWorkflows = function *(latestTotal) {
 module.exports.storeLatestTotal = function *(latestTotal) {
     let workflowTimingObject = new LeanCloud.WorkflowTiming();
     workflowTimingObject.set('latestTotal', latestTotal);
@@ -35,14 +31,12 @@ module.exports.storeLatestTotal = function *(latestTotal) {
 }
 
 /* workflow总数据量 */
-// module.exports.workflowTotal = function *() {
 module.exports.total = function *() {
     let workflowsQuery = new LeanCloud.AV.Query('Workflows');
     return workflowsQuery.count();
 }
 
 /* 指定时间点之后的爬取的workflow数据总数 */
-// module.exports.workflowSpiderDaily = function *(date) {
 module.exports.spiderDaily = function *(date) {
     let workflowQuery = new LeanCloud.AV.Query('Workflows');
     workflowQuery.greaterThan('createdAt', new Date(date));
