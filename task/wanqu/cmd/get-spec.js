0d6181089c7408ee8a886888fc5148c7b9919770
Mr.Krabs <pangxie.wdy@gmail.com>
2016-07-08 13:16:24 +0800

    refactor: 更新日志内容

--- task/wanqu/cmd/get-spec.js
+++ task/wanqu/cmd/get-spec.js
@@ -6,7 +6,7 @@ const wanquUtil = require('../wanqu-util');
 module.exports = function *(body, ctx) {
     if(!body || !body.issue) return {"success": false};
 
-    yield util.leanCloud.wanqu.log(util.getIP(ctx), `获取指定issue的文章, issue为: ${body.issue}`);
+    yield util.leanCloud.wanqu.log(util.getIP(ctx), `获取第${issue}期的文章`);
     /* 先判断DB中是否有缓存 */
     let dbData = yield util.leanCloud.wanqu.getSpec(body.issue);
     return wanquUtil.generateResponse(dbData);
