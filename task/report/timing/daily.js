9d76b623c5567fad7fd6d90481dc8486966057f5
Mr.Krabs <pangxie.wdy@gmail.com>
2016-07-08 22:40:00 +0800

    fix: 日志的日报数据获取失败bugfix

--- task/report/timing/daily.js
+++ task/report/timing/daily.js
@@ -5,11 +5,11 @@ const util = require('../../../util');
 /* 生成报表信息 */
 module.exports = function *(ctx) {
     let yesterday = util.getYesterday();
-    let wanquTotal = yield util.leanCloud.wanquTotal();
-    let workflowTotal = yield util.leanCloud.workflowTotal();
-    let wanquLogYesterday = yield util.leanCloud.wanquLogDaily(yesterday.toLocaleDateString() + ' 22:00:00');
-    let wanquSpiderYesterday = yield util.leanCloud.wanquSpiderDaily(yesterday.toLocaleDateString() + ' 22:00:00');
-    let workflowSpiderYesterday = yield util.leanCloud.workflowSpiderDaily(yesterday.toLocaleDateString() + ' 22:00:00');
+    let wanquTotal = yield util.leanCloud.wanqu.total();
+    let workflowTotal = yield util.leanCloud.workflows.total();
+    let wanquLogYesterday = yield util.leanCloud.wanqu.logDailyCount(yesterday.toLocaleDateString() + ' 22:00:00');
+    let wanquSpiderYesterday = yield util.leanCloud.wanqu.spiderDaily(yesterday.toLocaleDateString() + ' 22:00:00');
+    let workflowSpiderYesterday = yield util.leanCloud.workflows.spiderDaily(yesterday.toLocaleDateString() + ' 22:00:00');
 
     return {
         "access_count": wanquLogYesterday.length,
