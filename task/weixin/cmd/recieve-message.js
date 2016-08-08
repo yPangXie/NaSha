28551c87e29b654e7d0283a1ebebcd1785f25d7f
Mr.Krabs <pangxie.wdy@gmail.com>
2016-08-08 10:46:27 +0800

    feat: 支持weixin公众账号抓取页面信息

--- task/weixin/cmd/recieve-message.js
+++ task/weixin/cmd/recieve-message.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const cheerio = require('cheerio');
+const urllib = require('urllib');
+const util = require('../../../util');
+
 /* 校验来自微信服务器 */
 module.exports = function *(ctx) {
     let content = '';
@@ -15,7 +19,9 @@ module.exports = function *(ctx) {
         console.log('recieve message error:', e);
     }
 
-    /* TBD: 发来的是链接的时候, 做一些页面数据抓取和DB存储的操作 */
+    /* 发来的是链接的时候, 做一些页面数据抓取和DB存储的操作 */
+    let pageInformation = yield grabPageInfo(content);
+    yield util.leanCloud.read.store(pageInformation);
 
     return generateResponseXML({
         "responseToUserName": responseToUserName,
@@ -25,6 +31,30 @@ module.exports = function *(ctx) {
 ${content}`
             : "发生了一些不可描述的问题? 发来的数据里少了点儿什么.."
     });
+}
+
+/* 收到的指令是抓页面的时候, 执行该方法 */
+function *grabPageInfo (url) {
+    let pageData = urllib.requestThunk(url, {"timeout": 1000000});
+    let $ = cheerio.load(new Buffer(pageData.data).toString());
+
+    /* 先去获取`favicon`的相对路径(也许是绝对路径, 不重要) */
+    let faviconRelative = $('link[rel="shortcut icon"]').attr('href') || '';
+    let favicon = '';
+    if(faviconRelative) {
+        /* 转换为绝对路径 */
+        let anchor = document.createElement('a');
+        anchor.href = faviconRelative;
+        favicon = anchor.href || '';
+    }
+
+    return {
+        url: url,
+        favicon: favicon,
+        title: $('title').text() || '',
+        description: $('meta[name="description"]').attr('content') || '',
+        keywords: $('meta[name="keywords"]').attr('content') || ''
+    }
 }
 
 /* 生成返回值的`xml`结构 */
