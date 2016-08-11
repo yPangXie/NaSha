8d29ae6456b8c01ec862ff8e1dcd684770334bb6
Mr.Krabs <pangxie.wdy@gmail.com>
2016-08-11 17:44:42 +0800

    refactor: 调整写库操作. 先做数据判断.

--- task/read/cmd/store.js
+++ task/read/cmd/store.js
@@ -4,13 +4,12 @@ const readUtil = require('../util');
 const util = require('../../../util');
 
 /* 新增数据 */
-module.exports = function *(query, ctx) {
-    let url = query.url;
-    if(!url) return {"success": false, "message": "url呢?"};
+module.exports = function *(body, ctx) {
+    if(!body || !Object.keys(body.page || {}).length) return {"success": false, "message": "抓的数据乱七八糟.."};
 
-    let pageInformation = yield readUtil.grabPageInfo(url);
-    yield util.leanCloud.read.store(pageInformation);
+    let searchRet = yield util.leanCloud.read.searchByUrl(body.page.url);
+    if(searchRet && searchRet.length > 0) return {"success": false, "message": `"${searchRet[0].createdAt.toLocaleString()}" 已经保存过了<br />DB objectId: ${searchRet[0].id}`}
 
-    if(query.callback) return `${query.callback}({"success": true, "message": "应该是保存成功了.."})`
+    yield util.leanCloud.read.store(body.page);
     return {"success": true, "message": "应该是保存成功了.."};
 }
