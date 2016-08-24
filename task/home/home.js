06301c0a0a01bb0bac3ab1598fd96eb9c0fe0176
Mr.Krabs <pangxie.wdy@gmail.com>
2016-08-24 14:32:48 +0800

    feat: 用一个特别裸的页面, 展示read list.

--- task/home/home.js
+++ task/home/home.js
@@ -3,9 +3,22 @@
 const util = require('../../util');
 
 module.exports = function *() {
-    return yield this.render('/home/home', {
-        token: this.session.weibo.token || "",
-        appKey: this.state.config.weibo.app_key,
-        redirectURI: this.state.config.weibo.redirect_uri
+    let readList = yield util.leanCloud.read.list({
+        "limit": 100,
+        "offset": 0
     });
+    
+    readList.forEach(item => {
+        let createdAt = new Date(item.createdAt);
+        item.createdAt = `${createdAt.toLocaleDateString()} ${createdAt.toLocaleTimeString()}`;
+    });
+
+    return yield this.render('/home/home', {
+        "list": readList
+    });
+    // return yield this.render('/home/home', {
+    //     token: this.session.weibo.token || "",
+    //     appKey: this.state.config.weibo.app_key,
+    //     redirectURI: this.state.config.weibo.redirect_uri
+    // });
 }
