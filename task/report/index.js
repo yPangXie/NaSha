b111fe6859dc15926d5a40ea1ff98d48a9808298
Mr.Krabs <pangxie.wdy@gmail.com>
2016-09-10 13:49:31 +0800

    refactor: 移除文件

--- task/report/index.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-/* 暴露的定时方法 */
-module.exports.cmd = {
-    /* 每天的报告 */
-    "daily": require('./cmd/daily'),
-    /* 距离目标日期还有多少天 */
-    "countDown": require('./cmd/count-down')
-}
--- task/workflow/index.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-/* 暴露cmd方法 */
-module.exports.cmd = {
-    /* workflows文件下载及上传 */
-    "grabAndUpload": require('./cmd/upload-workflow'),
-    /* workflow爬虫 */
-    "spider": require('./cmd/spider')
-}
