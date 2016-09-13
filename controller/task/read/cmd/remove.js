192f90a8570bd2a9e607f9f2286593f2864ad612
Mr.Krabs <pangxie.wdy@gmail.com>
2016-09-13 18:55:43 +0800

    refactor: 调整下参数名

--- controller/task/read/cmd/remove.js
+++ controller/task/read/cmd/remove.js
@@ -4,7 +4,6 @@ const model = require(global.__nasha.APP_MODEL);
 
 /* 删除数据 */
 module.exports = function *(body, ctx) {
-    if(!body || !body.objectId) return {"success": false, "message": "传的什么参数.. objectId呢.."};
-
-    return yield model.leanCloud.read.remove(body.objectId);
+    if(!body || !body.id) return {"success": false, "message": "传的什么参数.. objectId呢.."};
+    return yield model.leanCloud.read.remove(body.id);
 }
