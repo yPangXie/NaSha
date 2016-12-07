2b99a78145e67ff7068dc5577759d8e4e6bbef62
Mr.Krabs <pangxie.wdy@gmail.com>
2016-12-07 17:24:49 +0800

    feat: 获取页面description, 补偿机制.

--- controller/task/read/cmd/store.js
+++ controller/task/read/cmd/store.js
@@ -1,10 +1,20 @@
 "use strict";
 
+const request = require('co-request');
 const readUtil = require('../util');
+const mercuryConfig = require(`${global.__nasha.APP_ROOT}/.config`).mercury;
 const model = require(global.__nasha.APP_MODEL);
 
 /* 新增数据 */
 module.exports = async (body = {}, ctx) => {
+    /* body.page的字段包含
+        host: 页面的host地址
+        favicon: favicon图片地址
+        url: 当前页面的完整url地址
+        title: 页面title信息(来自页面的title标签)
+        description: 页面的description信息(来自meta标签)
+        keywords: 页面的keywords数据(来自meta标签)
+    */
     if(!body.page) return {"success": false, "message": "抓的数据乱七八糟.."};
 
     let pageObject = {};
@@ -12,6 +22,20 @@ module.exports = async (body = {}, ctx) => {
         pageObject = JSON.parse(decodeURIComponent(body.page));
     } catch(e) {
         return {"success": false, "message": "数据格式异常, 跪了."}
+    }
+
+    /* 若页面的title或者description为空, 则尝试通过mercury的服务分析页面数据, 只能抓取出来 */
+    if(!pageObject.title || !pageObject.description) {
+        let smartPageInfo = await request({
+            "url": `${mercuryConfig.url}${pageObject.url}`,
+            "headers": {
+                "Content-Type": "application/json",
+                "x-api-key": mercuryConfig.token
+            }
+        });
+        let smartPageInfoObject = JSON.parse(smartPageInfo.body);
+        pageObject.title = pageObject.title || smartPageInfoObject.title || '';
+        pageObject.description = pageObject.description || smartPageInfoObject.excerpt || '';
     }
 
     let searchRet = await model.leanCloud.read.searchByUrl(pageObject.url);
